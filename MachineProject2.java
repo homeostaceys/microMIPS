44aa07e6e40621bd6450a7c18ec5d8c21d5f09da
homeostaceys <reyes.staceykyle@gmail.com>
2018-03-10 12:28:59 +0800

    Opcode for SD and LD

--- MachineProject2.java
+++ MachineProject2.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 public class MachineProject2 {
 
     /**	This method displays the main menu.
-	 */
+     */
     public void mainMenu() {
         System.out.println("What would you like to do?\n");
         System.out.println("[1] - Input Instruction(s)");
@@ -108,33 +108,25 @@ public class MachineProject2 {
     public String getInstOp(String inst) {
         String op = null;
         
-        switch(inst) {
-            case "LD":
-                op = "110111";
-                break;
-            case "SD":
-                op = "111111";
-                break;
-            case "DADDIU":
-                op = "011001";
-                break;
-            case "XORI":
-                op = "001110";
-                break;
-            case "DADDU":
-                op = "000000";
-                break;
-            case "SLT":
-                op = "000000";
-                break;
-            case "BGTZC":
-                op = "010111";
-                break;
-            case "J":
-                op = "000010";
-            default:
-                System.out.println("Invalid");
-        }
+        if("LD".equalsIgnoreCase("LD"))
+            op = "110111";
+        else if("SD".equalsIgnoreCase("SD"))
+            op = "110111";
+        else if("DADDIU".equalsIgnoreCase("DADDIU"))
+            op = "110111";
+        else if("XORI".equalsIgnoreCase("XORI"))
+            op = "110111";
+        else if("DADDU".equalsIgnoreCase("DADDU"))
+            op = "110111";
+        else if("SLT".equalsIgnoreCase("SLT"))
+            op = "110111";
+        else if("BGTZC".equalsIgnoreCase("BGTZC"))
+            op = "110111";
+        else if("J".equalsIgnoreCase("J"))
+            op = "110111";
+        else
+            System.out.println("Instruction not supported.");
+        
         return op;
     }
     
@@ -156,6 +148,75 @@ public class MachineProject2 {
     public String getImmOp(String imm) {
         return convHexToBin(imm);
     }
+    
+    public String getBaseOp(String base) {
+        return "A";
+    }
+    
+    public String getOffsetOp(String offset) {
+        return "B";
+    }
+    
+    /** This method prints the opcode for DADDIU or XORI
+     * 
+     * @param in is the instruction
+     * @param rs is the rs value
+     * @param rt is the rt value
+     * @param imm is the immediate value
+     */
+    public void Scenario1(String in, String rs, String rt, String imm) {
+        String opc = null;
+        //get binary
+        String inopp = getInstOp(in);
+        String rsopp = getRegOp(rs);
+        String rtopp = getRegOp(rt);
+        String immopp = getImmOp(imm);
+
+        //sign extend
+        rsopp = signExtend(rsopp);
+        rtopp = signExtend(rtopp);
+        immopp = signExtendImm(immopp);
+
+        //concatenate
+        opc = inopp;
+        opc += rsopp;
+        opc += rtopp;
+        opc += immopp;
+                        
+        //print in hex
+        System.out.println(convBinToHex(opc) + "\n");
+    }
+    
+    /** This method prints the opcode for SD or LD
+     * 
+     * @param in is the instruction
+     * @param base is the base value
+     * @param rt is the rt value 
+     * @param offset is the offset value
+     */
+    public void Scenario2(String in, String base, String rt, String offset) {
+        String opc = null;
+        //get binary
+        String inopp = getInstOp(in);
+        String baseopp = getBaseOp(base);
+        String rtopp = getRegOp(rt);
+        String offsetopp = getOffsetOp(offset);
+
+        //sign extend
+        baseopp = signExtend(baseopp);
+        rtopp = signExtend(rtopp);
+        offsetopp = signExtendImm(offsetopp);
+
+        //concatenate
+        opc = inopp;
+        opc += baseopp;
+        opc += rtopp;
+        opc += offsetopp;
+                        
+        //print in hex
+        System.out.println(convBinToHex(opc) + "\n");
+    }
+    
     /** This method checks whether the instruction is a valid instruction.
      * 
      * @param instrc is the instruction
@@ -237,7 +298,8 @@ public class MachineProject2 {
                rs = null, 
                rt = null,
                imm = null,
-               opc = null;
+               base = null,
+               offset = null;
         	   
         MachineProject2 m = new MachineProject2();
         Scanner sc = new Scanner (System.in);
@@ -260,64 +322,43 @@ public class MachineProject2 {
 //                    rs = "R5";
 //                    rt = "R20";
 //                    imm = "FFFF";
-                    System.err.print("Add an instruction: ");
-                	instr = new ArrayList<>();
-                	ctr = 0;
-                	instr.add(sc.nextLine());  
-                	while(errorCheck(instr.get(ctr))&&((instr.get(ctr).compareToIgnoreCase("END")) != 0))
-        			{
-        				instr.add(sc.nextLine());		
-        				ctr++;
-          			}
-        			
+                    instr = new ArrayList<>();
+                    ctr = 0;
+                    System.out.print("Add an instruction: ");
+                    instr.add(sc.nextLine());  
+                    while(errorCheck(instr.get(ctr))&&((instr.get(ctr).compareToIgnoreCase("END")) != 0)) {
+                        instr.add(sc.nextLine());
+                        ctr++;
+                    }
+                    
                     break;
                 case 2: /* Reset all register and memory values */
-                	memory = new String[65536][2];
-                	for(int i = 1; i <= 31; i++) {
-                		reg.add("0000000000000000");
-                		freg.add("0000000000000000");
-                	}
-                	
-                	for(int i = 0; i < 65535; i++) {
-                		memory[i][0] = Integer.toHexString(i);
-                		memory[i][1] = "00";
-                	}
+                    memory = new String[65536][2];
+                    for(int i = 1; i <= 31; i++) {
+                        reg.add("0000000000000000");
+                	freg.add("0000000000000000");
+                    }
+                    for(int i = 0; i < 65535; i++) {
+                	memory[i][0] = Integer.toHexString(i);
+                	memory[i][1] = "00";
+                    }
                 	
                     break;
                 case 3: /* Load values to tables */
-                	for(int i = 0; i < instr.size(); i++) {
-                		parseLoad(instr.get(i));
-                	}
+                    for(int i = 0; i < instr.size(); i++) {
+                	parseLoad(instr.get(i));
+                    }
                 	
                     break;
                 case 4: /* Start! */
-                    
+                    System.out.println("Opcode of MIPS Program:");
                     /* Scenario 1: DADDIU or XOR */
                     if(in != null && rs != null && rt != null && imm != null) {
-                        System.out.println("MIPS Program:");
-                        System.out.println(in + " " + rs + "," + rt + "," + imm + "\n");
-                        System.out.println("Opcode of MIPS Program:");
-
-                        //get binary
-                        String inopp = m.getInstOp(in);
-                        String rsopp = m.getRegOp(rs);
-                        String rtopp = m.getRegOp(rt);
-                        String immopp = m.getImmOp(imm);
-
-                        //sign extend
-                        rsopp = m.signExtend(rsopp);
-                        rtopp = m.signExtend(rtopp);
-                        immopp = m.signExtendImm(immopp);
-
-                        //concatenate
-                        opc = inopp;
-                        opc += rsopp;
-                        opc += rtopp;
-                        opc += immopp;
-                        
-                        //print in hex
-                        System.out.println(m.convBinToHex(opc) + "\n");
-                    } else
+                        m.Scenario1(in, rs, rt, imm);
+                    /* Scenario 2: LD or SD */
+                    } else if(in != null && rs != null && rt != null && imm != null) {
+                        m.Scenario2(in, base, rt, offset);
+                    }else
                         System.out.println("Invalid.\n");
                     
                     
