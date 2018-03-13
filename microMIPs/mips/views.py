cf943827f66f75074c63d452881f384d540dee04
kimmieco <kimberly_jane_co@dlsu.edu.ph>
2018-03-14 01:17:22 +0800

    new changes

--- microMIPs/mips/views.py
+++ microMIPs/mips/views.py
@@ -11,7 +11,8 @@ def index(request):
 
 def errorCheck(instr):
     regex = r"^((\w+:( )?)?((LD|SD) R([0-9]|1[0-9]|2[0-9]|3[0-1]),)( ([0-9A-F]){4})(\(R([0-9]|1[0-9]|2[0-9]|3[0-1])\)))$|^((\w+:( )?)?(DADDIU|XORI)( R([0-9]|1[0-9]|2[0-9]|3[0-1]),){2}( ((0x)|#)(([0-9A-F])){4}))$|^((\w+:( )?)?(DADDU|SLT)( R([0-9]|1[0-9]|2[0-9]|3[0-1]),){2}( R([0-9]|1[0-9]|2[0-9]|3[0-1])))$|^((\w+:( )?)?(BGTZC R([0-9]|1[0-9]|2[0-9]|3[0-1]),)( \w+))$|^((\w+:( )?)?(J \w+))$"
-    
-    if re.search(regex, instr):
+
+    if re.search(regex, instr): # need to add checking of labels
         return True
+
     return False
