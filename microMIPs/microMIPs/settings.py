9f7f6af37f39222253a85a23c69d982681a69344
homeostaceys <reyes.staceykyle@gmail.com>
2018-04-11 19:44:49 +0800

    edits

--- microMIPs/microMIPs/settings.py
+++ microMIPs/microMIPs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%%h^p*wt$**+qamc!$%053xf%dcdi!^26=1tc**6w8%ceqv8a0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
